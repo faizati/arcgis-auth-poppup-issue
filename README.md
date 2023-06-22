c5578527487e5101452b48305023cb2532dc8b3c
Faiz Asyraf Bin Abdul Aziz <fabdulaziz@bousteadgeospatial.com>
2023-06-22 10:18:10 +0800

    remove euclideon code ad readme file

---
+++ README.md
@@ -0,0 +1,4 @@
+# Steps to run code:
+- npm i
+- npm run start
+- open localhost:8888
--- app.js
+++ app.js
@@ -12,18 +12,6 @@ app.use((req, res, next) => {
         next();
 });
 
-// var corsCallback = function(req, res, next){
-//   res.header('Access-Control-Allow-Origin', '*'); 
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'Content-Length, Range, Content-Type, Authorization, Content-Length, X-Requested-With');
-//   res.header('Cross-Origin-Opener-Policy', 'same-origin');
-//   res.header('Cross-Origin-Embedder-Policy', 'require-corp');
-//   res.header('Cross-Origin-Resource-Policy', 'cross-origin');
-//   next();
-// }
-
-// router.options("*", corsCallback);
-
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
