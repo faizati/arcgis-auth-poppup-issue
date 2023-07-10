25f71a3df62f9370ab56b6d27979cfec22a6a07c
Faiz Asyraf Bin Abdul Aziz <fabdulaziz@bousteadgeospatial.com>
2023-07-10 10:35:49 +0800

    latest changes

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const router = express.Router();
+const path = require("path");
 
 app.use((req, res, next) => {
-      res.append("Access-Control-Allow-Origin", ["*"]);
-        res.append("Access-Control-Allow-Headers", ["Content-Length", "Range"]);
-        res.append('Cross-Origin-Opener-Policy', "same-origin");
-        res.append('Cross-Origin-Embedder-Policy', "credentialless ");
-        res.append('Cross-Origin-Resource-Policy', "cross-origin");
-        next();
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Headers", ["Content-Length", "Range"]);
+  res.append("Cross-Origin-Opener-Policy", "same-origin");
+  res.append("Cross-Origin-Embedder-Policy", "credentialless");
+  res.append("Cross-Origin-Resource-Policy", "cross-origin");
+  next();
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(8888);
