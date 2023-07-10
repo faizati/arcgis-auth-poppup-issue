bcecdde9361c7e54fc8ffa01e0d8044d1e269e5a
Faiz Asyraf Bin Abdul Aziz <fabdulaziz@bousteadgeospatial.com>
2023-07-10 10:37:53 +0800

    without cors

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["*"]);
-  res.append("Access-Control-Allow-Headers", ["Content-Length", "Range"]);
-  res.append("Cross-Origin-Opener-Policy", "same-origin");
-  res.append("Cross-Origin-Embedder-Policy", "credentialless");
-  res.append("Cross-Origin-Resource-Policy", "cross-origin");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.append("Access-Control-Allow-Origin", ["*"]);
+//   res.append("Access-Control-Allow-Headers", ["Content-Length", "Range"]);
+//   res.append("Cross-Origin-Opener-Policy", "same-origin");
+//   res.append("Cross-Origin-Embedder-Policy", "credentialless");
+//   res.append("Cross-Origin-Resource-Policy", "cross-origin");
+//   next();
+// });
 
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
